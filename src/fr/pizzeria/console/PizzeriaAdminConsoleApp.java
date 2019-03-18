d84977a4f0b9f641845a00d1049e39d49dc1dc35
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-03-18 14:16:27 +0100

    Menu complet et fonctionnel

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,6 +1,7 @@
 package fr.pizzeria.console;
 
 import java.util.Scanner;
+import fr.pizzeria.model.Pizza;
 
 public class PizzeriaAdminConsoleApp 
 {
@@ -10,6 +11,17 @@ public class PizzeriaAdminConsoleApp
 	{
 		int choice = 0;
 		questionUser = new Scanner (System.in);
+		
+		Pizza [] tableauPizza = new Pizza [100];
+		tableauPizza[0] = new Pizza ("PEP",  "Pépéroni", 	 12.50);
+		tableauPizza[1] = new Pizza ("MAR",  "Margherita", 	 14.00);
+		tableauPizza[2] = new Pizza ("REIN", "La Reine", 	 11.50);
+		tableauPizza[3] = new Pizza ("FRO",  "La 4 Fromage", 12.00);
+		tableauPizza[4] = new Pizza ("CAN",  "La cannibale", 12.50);
+		tableauPizza[5] = new Pizza ("SAV",  "La savoyarde", 13.00);
+		tableauPizza[6] = new Pizza ("ORI",  "L’orientale",  13.50);
+		tableauPizza[7] = new Pizza ("IND",  "L’indienne", 	 14.00);
+		int tailleTableau = 8;
 		
 		while (choice != 99)
 		{
@@ -26,20 +38,34 @@ public class PizzeriaAdminConsoleApp
 			if (choice == 1)
 			{
 				System.out.println("Liste des pizzas : ");
+				
+				for (Pizza pizza : tableauPizza)
+					if (pizza != null)
+						pizza.affichage();
+				
+				System.out.println(" ");	
 			}
 			else if (choice == 2)
 			{
 				System.out.println("Ajout d'une nouvelle pizza : ");
+				tableauPizza[tailleTableau] = new Pizza ();
+				tableauPizza[tailleTableau].modifPizza();
 			}
 			else if (choice == 3)
 			{
 				System.out.println("Mise à jour d'une pizza : ");
+				System.out.println("Veuillez choisir le code de la pizza à modifier : ");
+				choice = questionUser.nextInt();
+				tableauPizza[choice].modifPizza();
 			}
 			else if (choice == 4)
 			{
 				System.out.println("Suppression d'une pizza : ");
+				System.out.println("Veuillez choisir le code de la pizza à supprimer : ");
+				choice = questionUser.nextInt();
+				tableauPizza[choice] = null;
 			}
-			else if (choice == 4)
+			else if (choice == 99)
 			{
 				System.out.println("Aurevoir :(");
 			}
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -1,25 +1,102 @@
 package fr.pizzeria.model;
 
+import java.util.Scanner;
+
 public class Pizza 
 {
-	static int id = 0;
+	static int nbPizza = 0;
+	int id;
 	String code;
 	String libelle;
 	double prix;
 	
-	public Pizza (String code, String libelle, double prix) {
+	public Pizza () 
+	{
+		super ();
+		this.id = nbPizza;
+		nbPizza++;
+	}
+	
+	public Pizza (String code, String libelle, double prix) 
+	{
 		super();
-		Pizza.id++;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+		
+		this.id = nbPizza;
+		nbPizza++;
+	}
+	
+	public Pizza (int id, String code, String libelle, double prix) 
+	{
+		super();
+		this.id = id;
 		this.code = code;
 		this.libelle = libelle;
 		this.prix = prix;
 	}
 	
-	public Pizza (int id, String code, String libelle, double prix) {
-		super();
-		Pizza.id = id;
+	public int getId() 
+	{
+		return id;
+	}
+
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+
+	public String getCode() 
+	{
+		return code;
+	}
+
+	public void setCode(String code) 
+	{
 		this.code = code;
+	}
+
+	public String getLibelle() 
+	{
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) 
+	{
 		this.libelle = libelle;
+	}
+
+	public double getPrix() 
+	{
+		return prix;
+	}
+
+	public void setPrix(double prix) 
+	{
 		this.prix = prix;
+	}
+	
+	public void affichage ()
+	{
+		System.out.println(code + " -> " + libelle + " (" + prix + " €) ");
+	}
+	
+	public void modifPizza ()
+	{
+		Scanner questionUser = new Scanner (System.in);
+		
+		System.out.println("Veuillez saisir le code :");
+		String choiceString = questionUser.nextLine();
+		setCode (choiceString);
+		
+		System.out.println("Veuillez saisir le nom (sans espace):");
+		choiceString = questionUser.nextLine();
+		setLibelle (choiceString);
+		
+		System.out.println("Veuillez saisir le prix :");
+		double choicePrice = questionUser.nextDouble();
+		setPrix (choicePrice);
+		
 	}
 }
