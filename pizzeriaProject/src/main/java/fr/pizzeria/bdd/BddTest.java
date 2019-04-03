82f40a7bd6e74ab775f44c98abaa24ebdf4791d2
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-03 11:45:46 +0200

    Modification du pom pour fonctionner avec CleverCloud
    Mise à jour de la version MySQL en 8.0.11
    Modification du fichier de test BddTest.java pour se connecter à la base de donnée à distance.

--- pizzeriaProject/src/main/java/fr/pizzeria/bdd/BddTest.java
+++ pizzeriaProject/src/main/java/fr/pizzeria/bdd/BddTest.java
@@ -18,33 +18,39 @@ public class BddTest
 
 		// DriverManager.registerDriver(new Driver());
 		// Etape 1 - Chargement du pilote
-		Class.forName("org.mariadb.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		// Etape 2 - Définition de la JDBC URL
-		String jdbcUrl = "jdbc:mariadb://localhost:3307/bdd_mysql";
+		String jdbcUrl = "jdbc:mysql://bxnieqyqjpcgwd3f2wwb-mysql.services.clever-cloud.com:3306/bxnieqyqjpcgwd3f2wwb?useSSL=false";
 		
 		// Etape 3 - Création de la connexion
-		Connection uneConnexion = DriverManager.getConnection(jdbcUrl, "root", "");
+		Connection connexionBDD = DriverManager.getConnection(jdbcUrl, "u23oixvvtlomobsc", "lwW5IiUfLRDdIsxsqY7G");
+		connexionBDD.setAutoCommit(false);
 		
-		Statement st = uneConnexion.createStatement();
+		
+		//*
+		Statement st = connexionBDD.createStatement();
 		
 		// Etape 4 - exécution  de la requête
-		ResultSet rs = st.executeQuery("select * from CLIENTS");
+		//st.execute("CREATE TABLE deuxChamp ( CODE_PAYS VARCHAR(4) PRIMARY KEY, NOM_PAYS VARCHAR(30) )");
 		
 		// Etape 5 - exploitation des résultats
+		ResultSet rs = st.executeQuery("SELECT * FROM pizza");
 		while(rs.next()) {
 			
-			String titre = rs.getString("CODE_CLIENT");
+			String titre = rs.getString("CODE_PAYS");
+			String titre2 = rs.getString("nom_PAYS");
 			
-			System.out.println("CODE=" + titre);
+			System.out.println("CODE=" + titre + " NOM=" + titre2);
 		}
 
 		// Etape 6 - Fermeture des ressources
 		rs.close();
 		
 		st.close();
+		//*/
 		
-		uneConnexion.close();
+		connexionBDD.close();
 
 	}
 }
