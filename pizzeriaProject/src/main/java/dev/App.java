b9d27484ebc8bb5f1840d929de90a9e8ea57403e
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-04 15:59:37 +0200

    Refactorisation du code de App.java

--- pizzeriaProject/src/main/java/dev/App.java
+++ pizzeriaProject/src/main/java/dev/App.java
@@ -1,14 +1,6 @@
 package dev;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-
 import fr.pizzeria.console.*;
-import fr.pizzeria.model.Pizza;
 
 /**
  * Hello world!
@@ -20,19 +12,6 @@ public class App
 	
     public static void main( String[] args )
     {
-    	/*
-    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pizzeriaProject");
-    	EntityManager em = emf.createEntityManager();
-
-    	TypedQuery <Pizza> requete = em.createQuery("select p from Pizza p", Pizza.class);
-    	List <Pizza> listPizza = requete.getResultList();
-    	
-    	listPizza.forEach(t -> System.out.println(t));
-        
-        em.close ();
-        emf.close ();
-        */
-        
-        p.display ();
+    	p.display ();
     }
 }
