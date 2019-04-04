d8e6285f26ec10ce624546bac2ff9c9fbcf9da36
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-04 12:06:19 +0200

    Modification sur la classe CommandePizza
    Ajout de constructeurs et de getters/setters.

--- pizzeriaProject/src/main/java/fr/pizzeria/model/CommandePizza.java
+++ pizzeriaProject/src/main/java/fr/pizzeria/model/CommandePizza.java
@@ -20,4 +20,51 @@ public class CommandePizza
 	private int commande_id;
 	@Column
 	private int pizza_id;
+	
+	public CommandePizza ()
+	{
+	}
+
+	public CommandePizza(int commande_id, int pizza_id)
+	{
+		super();
+		this.commande_id = commande_id;
+		this.pizza_id = pizza_id;
+	}
+
+	/**
+	 * Getter
+	 * @return the commande_id
+	 */
+	public int getCommande_id()
+	{
+		return commande_id;
+	}
+
+	/**
+	 * Setter
+	 * @param commande_id the commande_id to set
+	 */
+	public void setCommande_id(int commande_id)
+	{
+		this.commande_id = commande_id;
+	}
+
+	/**
+	 * Getter
+	 * @return the pizza_id
+	 */
+	public int getPizza_id()
+	{
+		return pizza_id;
+	}
+
+	/**
+	 * Setter
+	 * @param pizza_id the pizza_id to set
+	 */
+	public void setPizza_id(int pizza_id)
+	{
+		this.pizza_id = pizza_id;
+	}
 }
